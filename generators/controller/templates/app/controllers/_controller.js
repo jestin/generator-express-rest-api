1076041baf53dc6085e86f87e5602f0c54af4bcd
Jestin Stoffel <jestin.stoffel@gmail.com>
2017-07-05 21:49:23 -0500

    Add method to test for dependencies on controllers and repositories

--- generators/controller/templates/app/controllers/_controller.js
+++ generators/controller/templates/app/controllers/_controller.js
@@ -12,5 +12,11 @@ function <%= controllerMethod.toLowerCase() %>(req, res, next) {
 };
 
 module.exports = function(deps) {
+	_checkDeps(deps);
 	return new <%= controllerClassName %>Controller(deps)
 };
+
+function _checkDeps(deps) {
+	// test dependencies for required functionality
+	// throw errors when tests fail
+}
--- generators/repository/templates/app/repositories/_repository.js
+++ generators/repository/templates/app/repositories/_repository.js
@@ -6,10 +6,16 @@ function get<%= repositoryClassName %>Data(id) {
 	return { id: id };
 }
 
-	<%= repositoryClassName %>Repository.prototype = {
-		get<%= repositoryClassName %>Data: get<%= repositoryClassName %>Data
-	};
+<%= repositoryClassName %>Repository.prototype = {
+	get<%= repositoryClassName %>Data: get<%= repositoryClassName %>Data
+};
 
 module.exports = function(deps) {
+	_checkDeps(deps);
 	return new <%= repositoryClassName %>Repository(deps);
 };
+
+function _checkDeps(deps) {
+	// test dependencies for required functionality
+	// throw errors when tests fail
+}
